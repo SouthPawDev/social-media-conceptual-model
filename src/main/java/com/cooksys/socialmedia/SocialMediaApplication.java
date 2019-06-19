c08c8f2fbdc0bc5177ce1bd2ce0ff22c66ea00de
unknown <pmartinsdev@gmail.com>
2019-06-19 10:28:25 -0500

    Created folders and base of project

---
+++ src/main/java/com/cooksys/socialmedia/SocialMediaApplication.java
@@ -0,0 +1,14 @@
+package com.cooksys.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableSwagger2
+@SpringBootApplication
+public class SocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApplication.class, args);
+	}
+
+}
